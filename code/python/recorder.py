bac696d1b572da4f770c5211374041e162ab9c41
datkinson <daa9@aber.ac.uk>
2012-11-25 12:51:05 +0000

    Attempted to fix unfinished line in serial stream issue

--- code/python/recorder.py
+++ code/python/recorder.py
@@ -1,11 +1,22 @@
 #Authors: Daniel Atkinson
 #Version: 0.1
-import serial
+from serial import *
+
 ser = serial.Serial('/dev/ttyUSB0', 57600) #create serial object set to ttyUSB0 at a baud rate of 57600
-try:
-    if ser.isOpen(): #check if serial connection is open
-        reading = ser.readline() #read a line from buffer "\n" is end of line character and must be recieved otherwise it will lock up
-        ser.write("recieved") #try and only get this to send if a message was actually recieved
-        print reading #display message recieved in terminal
-    else:
-        ser.open() #try and open serial connection
+global last_reading
+buffer = ''
+def recieve(ser):
+    buffer = buffer + ser.read(ser.inWaiting())
+    if '\n' in buffer:
+        lines = buffer.split('\n')
+        last_reading = lines[-2]
+        buffer = lines[-1]
+
+while True:
+    try:
+        if ser.isOpen(): #check if serial connection is open
+            recieve(ser) #read a line from buffer "\n" is end of line character and must be recieved otherwise it will lock up
+            ser.write("recieved") #try and only get this to send if a message was actually recieved
+            print last_reading #display message recieved in terminal
+        else:
+            ser.open() #try and open serial connection
